5c4d378197a0967cf50b82eafa0bb6a8a1d4d64f
PerecNeDorogo <hepidoc777@gmail.com>
2023-05-09 15:01:05 +0400

    fotter and header scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
 
 });
+
+window.onscroll = function showHeader() {
+  var header = document.querySelector('.header');
+  var header = document.querySelector('.menu');
+  if (window.pageYOffset > 20){
+    header.classList.add('fixed');
+  }
+  else{
+    header.classList.remove('fixed');
+  }
+};
